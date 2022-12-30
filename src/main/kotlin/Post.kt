eb45ea930176fb1f1cc990514508a11b4542c2f1
Margaret Grett <relevabor@mail.ru>
2022-12-30 12:52:29 +0300

    Add functional

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -13,16 +13,36 @@ data class Post(
 
 object WallService {
     private var posts = emptyArray<Post>()
+    private var lastId = 0
     fun add(post: Post): Post {
-        posts += post
+        posts += post.copy(id = ++lastId)
         return posts.last()
-
-
     }
+
+    fun changePost(newPost: Post): Boolean {
+        for ((index, post) in posts.withIndex()) {
+        if(post.id == newPost.id){
+            posts[index] = newPost.copy()
+            return true
+        }
+        }
+        return false
+    }
+
+    fun printArray() {
+        for (post in posts) {
+            println(post.text)
+        }
+    }
+
 }
 
 fun main() {
-    val post = Post(12, 122, 15, 1, 2, "Happy New Year", true, false, false, false)
-    print(post.text)
-
+    val post1 = Post(1, 122, 15, 1, 2, "Happy New Year", true, canOpenComment = true, false, false)
+    WallService.add(post1)
+    val post2 = Post(2,122,15,2,4,"Be careful",true,true,false,false)
+    WallService.add(post2)
+    WallService.printArray()
+    println(WallService.changePost(Post(2,122,15,2,5,"Dont worry", true,true,false,false)))
+    WallService.printArray()
 }
